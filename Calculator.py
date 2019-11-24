9875f4695707880199179858a8f93b0aa354674a
N0wonCityBo1 <57225764+N0wonCityBo1@users.noreply.github.com>
2019-11-25 04:02:18 +0900

    Add files via upload

--- Calculator.py
+++ Calculator.py
@@ -1,30 +1,27 @@
-#-*- coding:UTF-8-*-
+while True :
+    print("(a+b)(a-b)(a*b)(a/b) 방식으로 각 값을 따로 받아계산을하는 계산기입니다! 종료를 원한다면 연산자 입력에 '@'를 입력해주세요!")
+    a = int(input("첫번째숫자를 입력해주세요:"))
+    b = input("연산자를 입력해주세요:")
+    c = int(input("두번째 숫자를 입력해주세요:"))
 
+    what = ''
 
-
-print("(a+b)(a-b)(a*b)(a/b) 방식으로 각 값을 따로 받아계산을하는 계산기입니다!")
-a = int(input("첫번째숫자를 입력해주세요:"))
-b = input("연산자를 입력해주세요:")
-c = int(input("두번째 숫자를 입력해주세요:"))
-
-
-
-
-what = ''
-
-if b=='+':
-    what = a+c
-elif b == '-':
-    what = a-c   
-elif b == '*':
-    what = a*c 
-elif b == '/':
-    what = a/c 
-else:
-    print("+,-,*,/ 중 하나만 입력해주세요!!")   
-    
- 
-print(what)
-
-
+   
+    if b=='+':
+        what = a+c
+        print(what)
+    elif b == '-':
+        what = a-c  
+        print(what) 
+    elif b == '*':
+        what = a*c 
+        print(what)
+    elif b == '/':
+        what = a/c 
+        print(what)
+    elif b == '@':
+        print("종료합니다.")
+        break    
+    else:
+         print("+,-,*,/ 중 하나만 입력해주세요!!") 
 
